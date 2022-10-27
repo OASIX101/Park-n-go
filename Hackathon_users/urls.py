771943378b70da6779cdbfc1d822d908760f0e80
unknown <anthonyolowuxx6@gmail.com>
2022-10-27 20:42:46 +0100

    gooo

--- Hackathon_users/urls.py
+++ Hackathon_users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.login_view),
     path('logout/', views.logout_view),
     path('register/', views.RegisterView().as_view()),
-    path('email-verify/', views.VerifyEmail.as_view(), name="email_verify"),
+    path('email-verify/', views.VerifyEmail.as_view(), name="email-verify"),
     path('vehicle/', views.add_vehicle, name="vehicle"),
     path('vehicle/<int:vehicle_id>/', views.VehicleEdit().as_view(), name="vehicle"),
     path('vehicles/', views.get_all_vehicle, name="all_vehicles"),
